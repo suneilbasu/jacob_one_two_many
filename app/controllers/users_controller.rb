cd615cba1f14ca9555165ce7d43a9e1cd9f40048
Suneil Basu <suneil@futureprogress.com>
2016-04-16 11:56:36 +0100

    tampering

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,11 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
     def index
     	@users = User.all
+
     end
     def show
     	@user = User.find(params[:id])
+      @devices = Device.all
 
     end
     def edit
